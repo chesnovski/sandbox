5a92330e598ddbb83c18de5cecb62b51e84aaf97
Fedor <chesnovski99@gmail.com>
2023-08-13 17:53:19 +0200

    Number people in the bus completed

---
+++ codewars/7kyu/NumberofPeopleintheBus.js
@@ -0,0 +1,28 @@
+// GOAL
+// There is a bus moving in the city which takes and drops some people at each bus stop.
+
+// You are provided with a list (or array) of integer pairs. Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+
+// Your task is to return the number of people who are still on the bus after the last bus stop (after the last array). Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+// Take a look on the test cases.
+
+// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
+
+// The second value in the first pair in the array is 0, since the bus is empty in the first bus stop.
+
+
+//SOLUTION
+
+
+function busstop(array) {
+
+    const groupedData = array.flat()
+    let oddIndexArr = groupedData.filter((_, i) => i % 2 !== 0).reduce((a,b) => a+b)
+    let nOddIndexArr = groupedData.filter((_, i) => i % 2 === 0).reduce((a,b) => a+b)
+    console.log(nOddIndexArr-oddIndexArr)
+
+}
+
+const array = [[3,0],[9,1],[4,10],[-12,2],[6,1],[7,10]]
+console.log(busstop(array))
