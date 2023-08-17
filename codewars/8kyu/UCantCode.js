c8f8adc1b1a000c4ac4ff5f56bec66bf7aafaef1
Fedor <chesnovski99@gmail.com>
2023-08-17 17:25:58 +0200

    Double completed

---
+++ codewars/8kyu/UCantCode.js
@@ -0,0 +1,10 @@
+//GOAL
+// Code as fast as you can! You need to double the integer and return it.
+
+// SOLUTION
+
+function doubleInteger(i) {
+    return Number(2*i)
+  }
+
+  console.log(doubleInteger(94))
