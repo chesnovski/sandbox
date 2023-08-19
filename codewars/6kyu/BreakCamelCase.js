20b1e7ada608bb8133123d68bc77098a1f3e9c80
Fedor <chesnovski99@gmail.com>
2023-08-19 16:49:17 +0200

    Break Camel Case completed

---
+++ codewars/6kyu/BreakCamelCase.js
@@ -0,0 +1,18 @@
+//GOAL 
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+//SOLUTION
+
+function solution(string) {
+
+    return (string.split(/(?=[A-Z])/)).join(' ')
+    
+}
+
+
+console.log(solution("camelCasing"))
