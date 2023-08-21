f345df6ded98403d8938ea88613a794d8345c588
Fedor <chesnovski99@gmail.com>
2023-08-21 21:07:21 +0200

    Find the unique string added reduce method

--- codewars/6kyu/FindTheUniqueString.js
+++ codewars/6kyu/FindTheUniqueString.js
@@ -1,7 +1,12 @@
 function findUniq(arr) {
-    let newArr = arr.map(a => { return [...new Set(a.toLowerCase())].sort().join('') });
-    return newArr
-  }
+    let newArr = arr.map(el => { return [...new Set(el.toLowerCase())].sort().join('') });
+  const result = newArr.reduce((acc, curr, currentIndex)=> {
+    if (newArr.indexOf(newArr[currentIndex]) === newArr.lastIndexOf(newArr[currentIndex])) return arr[currentIndex]
+
+  })
+  return result
+
+}
 
 
 console.log(findUniq([ 'silvia', 'vasili', 'victor' ]))
