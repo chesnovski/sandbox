300c6768da1eebd6b23752aa7a6e4e5038e033af
Fedor <chesnovski99@gmail.com>
2023-08-22 20:00:40 +0200

    Simple Fun completed

---
+++ codewars/7kyu/SimpleFun.js
@@ -0,0 +1,34 @@
+//GOAL
+// Let's define a parameter of number n as the least common multiple (LCM) of the sum of its digits and their product.
+
+// Calculate the parameter of the given number n.
+
+// Input/Output
+// [input] integer n
+
+// A positive integer. It is guaranteed that no zero appears in n.
+
+// [output] an integer
+
+// The parameter of the given number.
+
+// Example
+// For n = 22, the output should be 4.
+
+// Both the sum and the product of digits equal 4, and LCM(4, 4) = 4.
+
+// For n = 1234, the output should be 120.
+
+// 1+2+3+4=10 and 1*2*3*4=24, LCM(10,24)=120
+
+//SOLUTION
+
+function parameter(n) {
+    const gcd = (a,b) => b ? gcd(b,a%b) : a
+    const lcm = (a,b) => a*b/gcd(a,b)
+    let [p,s] = [...n+``].map(Number).reduce(([p,s],d) => [p*d,s+d], [1,0])
+    return lcm(p,s)
+  }
+
+
+console.log(parameter(22))
